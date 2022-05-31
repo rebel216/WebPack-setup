3aa9081f0a32fbae7e55434cee0c3a009bda9068
rebel216 <neerajbhardwajpkt@gmail.com>
2022-06-01 00:46:30 +0530

    Webpack dev server support

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,20 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    devtool: 'inline-source-map',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static:'./dist',
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -15,6 +25,10 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean:true,
+    },
+    optimization: {
+      runtimeChunk:'single'  
     },
     module: {
         rules: [
